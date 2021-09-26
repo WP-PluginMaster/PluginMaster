2944dd821688ee832f6dea7076ea7b888f9b9627
AL EMRAN <emrancu1@gmail.com>
2021-09-27 00:08:19 +0600

    update namespace

--- plugin-master.php
+++ plugin-master.php
@@ -1,26 +1,16 @@
 <?php
 
+
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://alemran.me/
- * @since             1.0.0
- * @package           WP Plugin Development FW
- *
- * @wordpress-plugin
- * Plugin Name:       PluginMaster
- * Plugin URI:        https://github.com/emrancu/wordpressPlugin
- * Description:       This is a framework for developing WordPress Plugin.
- * Version:           1.0.0
- * Author:            AL EMRAN
- * Author URI:        http://alemran.me/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Plugin Name: Plugin Master
+ * Plugin URI: https://wppluginmaster.com
+ * Description: PluginMaster is a Plugin Development Framework
+ * Version: 1.0.1
+ * Author: Plugin Master
+ * Author URI: https://wppluginmaster.com
+ * License:  GPL-3.0
+ * License URI:  http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain: Plugin Master
  */
 
 
@@ -31,7 +21,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 require_once 'vendor/autoload.php';
 
 
-use UIDom\Elementor\Bootstrap\Bootstrap;
+use PluginMaster\Bootstrap\Bootstrap;
 
 /**
  * Run Application
