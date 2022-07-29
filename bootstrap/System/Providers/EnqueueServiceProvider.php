c1ff7d844db8bade5ff0bc3ce7b34d0792f25b4b
AL EMRAN <emrancu1@gmail.com>
2022-07-29 23:27:02 +0600

    Update Enqueue Service Provider for Enqueue Object

--- bootstrap/System/Providers/EnqueueServiceProvider.php
+++ bootstrap/System/Providers/EnqueueServiceProvider.php
@@ -5,6 +5,7 @@ namespace PluginMaster\Bootstrap\System\Providers;
 
 
 use PluginMaster\Bootstrap\System\Application;
+use PluginMaster\Bootstrap\System\Enqueue;
 use PluginMaster\Bootstrap\System\Helpers\App;
 use PluginMaster\Contracts\Provider\ServiceProviderInterface;
 use PluginMaster\Foundation\Enqueue\EnqueueHandler;
@@ -20,14 +21,17 @@ class EnqueueServiceProvider implements ServiceProviderInterface
         /** @var EnqueueHandler $enqueueHandler */
         $enqueueHandler = $app->get(EnqueueHandler::class);
 
+        /** @var  Enqueue $enqueue */
+        $enqueue = $app->get(Enqueue::class);
+
         $enqueueHandler->setAppInstance($app)->loadEnqueueFile($app->enqueuePath('enqueue.php'));
 
-        add_action('admin_enqueue_scripts', function () use ($enqueueHandler) {
-            $enqueueHandler->initEnqueue();
+        add_action('admin_enqueue_scripts', function () use ($enqueueHandler, $enqueue) {
+            $enqueueHandler->initEnqueue($enqueue);
         });
 
-        add_action('wp_enqueue_scripts', function () use ($enqueueHandler) {
-            $enqueueHandler->initEnqueue();
+        add_action('wp_enqueue_scripts', function () use ($enqueueHandler, $enqueue) {
+            $enqueueHandler->initEnqueue($enqueue);
         }, 100);
     }
 
