bdfc20c298c6b2862244396773538bd1500e117e
AL EMRAN <emrancu1@gmail.com>
2022-07-29 22:56:06 +0600

    Update EnqueueServiceProvider.php

--- bootstrap/System/Providers/EnqueueServiceProvider.php
+++ bootstrap/System/Providers/EnqueueServiceProvider.php
@@ -4,6 +4,7 @@
 namespace PluginMaster\Bootstrap\System\Providers;
 
 
+use PluginMaster\Bootstrap\System\Application;
 use PluginMaster\Bootstrap\System\Helpers\App;
 use PluginMaster\Contracts\Provider\ServiceProviderInterface;
 use PluginMaster\Foundation\Enqueue\EnqueueHandler;
@@ -13,16 +14,20 @@ class EnqueueServiceProvider implements ServiceProviderInterface
 
     public function boot(): void
     {
+        /** @var Application $app */
         $app = App::get();
 
-        $app->get(EnqueueHandler::class)->setAppInstance($app)->loadEnqueueFile($app->enqueuePath('enqueue.php'));
+        /** @var EnqueueHandler $enqueueHandler */
+        $enqueueHandler = $app->get(EnqueueHandler::class);
 
-        add_action('admin_enqueue_scripts', function () use ($app) {
-            $app->get(EnqueueHandler::class)->initEnqueue(true);
+        $enqueueHandler->setAppInstance($app)->loadEnqueueFile($app->enqueuePath('enqueue.php'));
+
+        add_action('admin_enqueue_scripts', function () use ($enqueueHandler) {
+            $enqueueHandler->initEnqueue();
         });
 
-        add_action('wp_enqueue_scripts', function () use ($app) {
-            $app->get(EnqueueHandler::class)->initEnqueue();
+        add_action('wp_enqueue_scripts', function () use ($enqueueHandler) {
+            $enqueueHandler->initEnqueue();
         }, 100);
     }
 
