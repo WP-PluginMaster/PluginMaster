51987a64760e2c1d73a5ae2797dfc4b4a97010d1
AL EMRAN <emrancu1@gmail.com>
2022-08-04 01:53:04 +0600

    Add Shortcode to Index view page

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,5 +1,6 @@
-<h1 class="title-pm"> <?php
-    echo $title ?> </h1>
-
+<h1 class="title-pm">
+    <?php echo $title ?>  <a href=""> read more</a>
+</h1>
+<h4<?php echo do_shortcode( '[plugin-master]')  ?></h4>
 <div id="VueApp"></div>
 <div id="app"></div>
