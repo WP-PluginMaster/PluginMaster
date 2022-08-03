a20d8c5dd8f1826d7c976258139c96ce9e3bd13c
AL EMRAN <emrancu1@gmail.com>
2022-08-04 01:54:57 +0600

    Update sidemeu code

--- routes/sidemenu.php
+++ routes/sidemenu.php
@@ -4,9 +4,8 @@
 use PluginMaster\Bootstrap\System\SideMenu;
 
 
-SideMenu::parent('Demo Plugin')
-    ->menuTitle("Home | Demo Plugin")
-    ->callback('SideMenuController@index')
-    ->child('demo-plugin-home')->callback('SideMenuController@index') ;
+SideMenu::parent('PluginMaster')
+    ->menuTitle("Home | Plugin Master")
+    ->child('Home','pluginmaster-home')->callback('SideMenuController@index') ;
 
-SideMenu::submenu('demo-plugin', 'about')->callback('SideMenuController@about');
+SideMenu::submenu('pluginmaster', 'About', 'pluginmaster-about')->callback('SideMenuController@about');
