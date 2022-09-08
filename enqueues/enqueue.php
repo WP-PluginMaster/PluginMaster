020556d303dc4f345a612058be3b7046881a9f26
AL EMRAN <emrancu1@gmail.com>
2022-09-08 21:39:44 +0600

    Add js file and update enqueue

--- enqueues/enqueue.php
+++ enqueues/enqueue.php
@@ -4,7 +4,7 @@ use PluginMaster\Bootstrap\System\Enqueue;
 
 Enqueue::admin()->script('js/app.js')->inFooter();
 
-Enqueue::admin()->script('css/index.css')->version('123212')->inFooter();
+Enqueue::admin()->script('css/index.css')->version(time())->inFooter();
 
 
 
