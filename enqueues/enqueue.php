a332a7c0f2640bb4ffb29484e1fa0077f8b0ab32
AL EMRAN <emrancu1@gmail.com>
2022-09-11 12:06:52 +0600

    Update enqueue.php

--- enqueues/enqueue.php
+++ enqueues/enqueue.php
@@ -5,7 +5,7 @@ use PluginMaster\Bootstrap\System\Enqueue;
 Enqueue::admin()->script('js/react/app.js')->inFooter();
 Enqueue::admin()->script('js/js/vue/app.js')->inFooter();
 
-Enqueue::admin()->script('css/app.css')->version(time())->inFooter();
+Enqueue::admin()->style('css/app.css')->version(time());
 
 
 
